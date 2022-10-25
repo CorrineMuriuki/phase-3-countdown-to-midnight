f8725979fc0efa671d4a4e53ba2f32751ecb5571
CorrineMuriuki <Corrine.Muriuki@student.moringaschool.com>
2022-10-25 11:11:24 +0300

    Completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
+require 'pry'
 
-def countdown
+int = 10
+
+def countdown(int)
+
+    while int > 0
+        puts "#{int}  (n) SECOND(S)!"
+        return int
+        int -= 1
+    end
+     "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (int)
+    while int > 0
+        puts "#{int} SECOND(S)!"
+        sleep(1)
+        int -= 1
+    end
+     "HAPPY NEW YEAR!"
+end
+
+
